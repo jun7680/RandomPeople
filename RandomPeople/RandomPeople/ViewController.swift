4d1e820a2ff912dc10a68715b1c878bd234fa9fe
옥인준 <injunock@mailplug.co.kr>
2023-01-29 21:17:50 +0900

    Project init

---
+++ RandomPeople/RandomPeople/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPeople
+//
+//  Created by 옥인준 on 2023/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
