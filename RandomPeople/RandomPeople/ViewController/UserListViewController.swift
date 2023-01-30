d6f92fac75c64408d2d93cfe455f12c0346ee210
옥인준 <injunock@mailplug.co.kr>
2023-01-30 23:33:42 +0900

    Update ViewController name

---
+++ RandomPeople/RandomPeople/ViewController/UserListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RandomPeople
+//
+//  Created by 옥인준 on 2023/01/29.
+//
+
+import UIKit
+
+class UserListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
